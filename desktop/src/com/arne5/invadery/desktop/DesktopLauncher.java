488f8276fd476e781634b8c5b113550a9fb8d3cc
Dave Arneson <urvaius@gmail.com>
2015-06-03 15:57:35 -0700

    Initial commit

---
+++ desktop/src/com/arne5/invadery/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.arne5.invadery.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.arne5.invadery.InvaderY;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new InvaderY(), config);
+	}
+}
